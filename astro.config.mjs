0749a9ead4d46d195c1467c74e76cf567070c3f6
Zoltan Lajtavari <zoltan.lajtavari@idealo.de>
2024-10-21 17:16:17 +0200

    fix: fixes paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // If you're hosting under /zoltan-on-software on GitHub Pages:
+    base: '/zoltan-on-software/',
+    trailingSlash: "never"
+
+    // Other configuration options...
+});
