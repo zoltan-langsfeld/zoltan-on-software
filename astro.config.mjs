94a0b8774d2fc368083b38beb83bde7c52c611ac
Zoltan Lajtavari <zoltan.lajtavari@idealo.de>
2024-10-24 15:14:48 +0200

    chore: adds React

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    // If you're hosting under /zoltan-on-software on GitHub Pages:
-    base: '/zoltan-on-software/',
-    trailingSlash: "never"
+  // If you're hosting under /zoltan-on-software on GitHub Pages:
+  base: '/zoltan-on-software/',
 
-    // Other configuration options...
+  // Other configuration options...
+  trailingSlash: "never",
+
+  integrations: [react()]
 });
