d80012246f284225c08104a1fffe577a4eca9515
Newmomav <lipaavar@msn.com>
2025-04-15 00:01:24 +0200

    feat: add  astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
+import icon from "astro-icon";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +12,8 @@ export default defineConfig({
   // Other configuration options...
   trailingSlash: "never",
 
-  integrations: [react()],
+  integrations: [react(),icon()],
+
   vite: {
     plugins: [tailwindcss()],
   },
