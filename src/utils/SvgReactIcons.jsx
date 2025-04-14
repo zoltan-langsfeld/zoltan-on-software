c32f1c7b2c2e1812200988472f9c5d0366ee6455
Newmomav <lipaavar@msn.com>
2025-04-15 00:00:54 +0200

    feat: add icons to sidebar and component for svg rendering in react

---
+++ src/utils/SvgReactIcons.jsx
@@ -0,0 +1,27 @@
+const SvgIcon = ({ name, className }) => {
+  const icons = {
+    menu: (
+      // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className={className}
+      >
+        <line x1="4" x2="20" y1="12" y2="12" />
+        <line x1="4" x2="20" y1="6" y2="6" />
+        <line x1="4" x2="20" y1="18" y2="18" />
+      </svg>
+    ),
+  };
+
+  return icons[name] || null;
+};
+
+export default SvgIcon;
