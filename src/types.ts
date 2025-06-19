4378467f090cc425ccdd32fc6b8f6d649be3e574
Newmomav <lipaavar@msn.com>
2025-06-19 11:18:11 +0200

    add new about section and json resume

--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,54 @@ export type SocialLink = {
     href: string;
     label: string;
 };
+
+type ISODateString = string;
+
+export type Resume = {
+  summary: string;
+  experience: ExperienceEntry[];
+  education: EducationEntry[];
+  certifications: Certification[];
+  volunteering: Volunteering[];
+};
+
+export type ExperienceEntry = {
+  company: string;
+  location: string;
+  positions: {
+    title: string;
+    team?: string;
+    startDate: ISODateString;
+    endDate: ISODateString;
+    description?: string[];
+    skills: string[];
+  }[];
+};
+
+export type EducationEntry = {
+  institution: string;
+  subject: string;
+  location?: string;
+  degree?: string;
+  startDate: ISODateString;
+  endDate: ISODateString;
+  thesis?: string;
+  courses?: string[];
+  note?: string;
+};
+
+export type Certification = {
+    name: string;
+    issuer: string;
+    date: ISODateString;
+    validUntil?: ISODateString;
+    credentialUrl?: string;
+};
+
+export type Volunteering = {
+    organization: string;
+    position: string;
+    startDate: ISODateString;
+    endDate: ISODateString;
+    description?: string;
+};
