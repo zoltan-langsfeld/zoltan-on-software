d3b0a7f209ee6e8f696eec79d15aa84e17bec124
newmomav <lipaavar@msn.com>
2025-07-14 11:44:39 +0200

    add theme toggle

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+const ThemeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // useEffect(() => {
+  //     const browserTheme = localStorage.getItem('theme');
+  //     if (browserTheme === 'dark') {
+  //         setIsDarkMode(true);
+  //         document.documentElement.classList.add('dark');
+  //     }
+  // }, []);
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme === "dark") {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else if (storedTheme === "light") {
+      setIsDarkMode(false);
+      document.documentElement.classList.remove("dark");
+    } else {
+      const prefersDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      setIsDarkMode(prefersDark);
+      document.documentElement.classList.toggle("dark", prefersDark);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = !isDarkMode;
+    setIsDarkMode(newTheme);
+    localStorage.setItem("theme", newTheme ? "dark" : "light");
+    document.documentElement.classList.toggle("dark", newTheme);
+  };
+
+  return (
+    <div className="md:basis-1/3 flex justify-end">
+      <button className="w-4 h-4 md:w-6 md:h-6 rounded-full bg-primary-text" onClick={toggleTheme}/>
+    </div>
+  );
+};
+
+export default ThemeToggle;
