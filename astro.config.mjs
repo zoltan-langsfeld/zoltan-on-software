8f2dfec089c4b3a8bd64c936fd74b23424a0c976
newmomav <lipaavar@msn.com>
2025-07-14 11:42:19 +0200

    add shiki syntax highlighter for nice blog entries with code examples

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 import icon from "astro-icon";
-
 
 // https://astro.build/config
 export default defineConfig({
   // If you're hosting under /zoltan-on-software on GitHub Pages:
-  base: '/zoltan-on-software',
+  base: "/zoltan-on-software",
 
   // Other configuration options...
   trailingSlash: "never",
 
-  integrations: [react(),icon()],
+  integrations: [react(), icon()],
 
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "nord",
+    },
   },
 });
